d00f96764382bac3bd796fcb11788aa4950c07cc
hejianbo <hejianbo@spacezhu.cn>
2022-10-12 17:34:53 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Container from '@/pages/container'
 import Login from '@/pages/login'
 
 const App: FC = (props) => (
-  <Router>
+  <BrowserRouter basename="admin">
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Container {...props} />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 )
 
 export default App
